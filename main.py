607add5af2b0ddd54e2826e4a0827130aa358100
Zubair Siddiqui <60069297+sodmzs@users.noreply.github.com>
2023-05-18 12:50:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ import numpy as np
 from PIL.Image import Image
 import io
 import base64
+import os
 
 filename = "man_with_gun.png"
 instrument_file = "guitar.png"
-temp_path = "Temp/"
+temp_path = "Temp"
+
+isExist = os.path.exists(temp_path)
+if not isExist:
+   os.makedirs(temp_path)
+
 
 config = ConfigParser()
 print (config.read('config.ini'))
 
 res = detect_gun(filename)
-cv2.imwrite(temp_path+"gun_detected.png",res)
+cv2.imwrite(temp_path+"/gun_detected.png",res)
 
-file, msg = inpainting_detected_gun(filename,temp_path+"gun_detected.png")
+file, msg = inpainting_detected_gun(filename,temp_path+"/gun_detected.png")
 print("!!! ",msg)
 
-file, msg = smile_expression(temp_path+file)
+file, msg = smile_expression(temp_path+"/"+file)
 print("!!! ",msg)
 
-res = replacing_gun_with_musical_instrument(temp_path+file,instrument_file)
+res = replacing_gun_with_musical_instrument(temp_path+"/"+file,instrument_file)
 cv2.imwrite("final.png",res)
   
 
